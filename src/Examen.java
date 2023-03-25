de9e2fbcc71c2ab1ea7b27b2bfd2c1e7f28741cf
Samantha <samyguca@hotmail.com>
2023-03-25 09:50:02 -0600

    examen

---
+++ src/Examen.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Examen {
+    public static void main(String[] args)
+    {
+        List alumnos = new ArrayList<>();
+        alumnos.add("Juan");
+        alumnos.add("Leonardo");
+        alumnos.add("Patricia");
+        alumnos.add("Diego");
+        alumnos.add("Elizabeth");
+        alumnos.add("Maria");
+        alumnos.add("Rosa");
+        alumnos.add("Luis");
+        alumnos.add("Enrique");
+        System.out.println("El nombre de los alumnos son:");
+        System.out.println(alumnos);
+    }
+}
